206fd4553c558581bae0fe31468f99bc02ffa683
Saikat <saikat25112000@gmail.com>
2024-07-12 18:59:39 +0600

    About section done

--- script.js
+++ script.js
@@ -51,3 +51,28 @@ const typed = new Typed('.multiple-text',{
     backDelay: 1000,
     loop: true,
 });
+
+// switch between about buttons 
+
+const buttons = document.querySelectorAll('.about-btn button');
+const contents = document.querySelectorAll('.content');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    contents.forEach(content => content.style.display = 'none');
+    contents[index].style.display = 'block';
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+// portfolio fillter 
+
+var mixer = mixitup('.portfolio-gallery',{
+    selectors: {
+        target: '.portfolio-box'
+    },
+    animation: {
+        duration: 500
+    }
+});
